bc26a459b400bb93663c687c4c1cb0f8b54f07a6
KatmannikCore <vlad.urbanovitch@yandex.by>
2022-03-18 20:17:16 +0300

    inst fix

--- script.js
+++ script.js
@@ -13,14 +13,3 @@ btn.addEventListener('click', function() {
     btn.innerHTML = 'Читать полность';
   }
 })
-function isMobile() {
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-    let companies = document.querySelectorAll('.manufacturers > div img');
-    let arrayNumber = ['09', '10', '05', '04', '03', '02', '01', '08', '07', '06']
-    for (let i = 0; i < arrayNumber.length; i++) {
-      companies[i].src = `img/reviews/manufacturers/${arrayNumber[i]}.png`
-      
-    }
-  } 
-}
-isMobile()
