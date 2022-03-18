3ea70c96b2990838ccd0d5ed26bc4d79406e3faa
KatmannikCore <vlad.urbanovitch@yandex.by>
2022-03-18 21:34:08 +0300

    hidden text demo

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ btn.addEventListener('click', function() {
     btn.innerHTML = 'Читать полность';
   }
 })
+const btn2 = document.getElementById('hideBtn2');
+const hidden_text2 = document.querySelector('.gradient_hidden2');
+btn2.addEventListener('click', function() {
+  hidden_text2.classList.toggle('gradient_hidden2');
+  document.getElementById('text2').classList.toggle('hide-and-show');
+  if (btn.innerHTML === 'Читать полностью') {
+    btn.innerHTML = 'Скрыть текст';
+  } else {
+    btn.innerHTML = 'Читать полность';
+  }
+})
