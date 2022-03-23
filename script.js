8a9d665ffaee9525a848bd77695562001088e83f
KatmannikCore <vlad.urbanovitch@yandex.by>
2022-03-23 16:03:55 +0300

    fix designer first

--- script.js
+++ script.js
@@ -6,7 +6,9 @@
           document.querySelectorAll('.nav > ul > li').forEach(item => {item.classList.toggle('li_active')})
       })
   })
-  
+  document.querySelectorAll('.show_more')[0].addEventListener('click', ()=>{
+    document.querySelector('.container_add').classList.toggle('none_cont');
+  })
 const btn = document.getElementById('hideBtn');
 const hidden_text = document.querySelector('.gradient_hidden');
 if(window.innerWidth > 750){
@@ -22,14 +24,16 @@ btn.addEventListener('click', function() {
     btn.innerHTML = 'Читать полность';
   }
 })
-const btn2 = document.getElementById('hideBtn2');
-const hidden_text2 = document.querySelector('.gradient_hidden2');
-btn2.addEventListener('click', function() {
-  hidden_text2.classList.toggle('gradient_hidden2');
-  document.getElementById('text2').classList.toggle('hide-and-show');
-  if (btn.innerHTML === 'Читать полностью') {
-    btn.innerHTML = 'Скрыть текст';
-  } else {
-    btn.innerHTML = 'Читать полность';
-  }
-})
+// const btn2 = document.getElementById('hideBtn2');
+// const hidden_text2 = document.querySelector('.gradient_hidden2');
+// btn2.addEventListener('click', function() {
+//   hidden_text2.classList.toggle('gradient_hidden2');
+//   document.getElementById('text2').classList.toggle('hide-and-show');
+//   if (btn.innerHTML === 'Читать полностью') {
+//     btn.innerHTML = 'Скрыть текст';
+//   } else {
+//     btn.innerHTML = 'Читать полность';
+//   }
+// })
+
+//none_cont
